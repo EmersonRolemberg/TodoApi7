cadb32776677d467500d7957f21093d40b1c1306
EmersonRolemberg <115815559+EmersonRolemberg@users.noreply.github.com>
2023-03-23 09:08:45 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import tarefasController from "./controllers/tarefasController.js";
+import usuariosController from "./controllers/usuariosController.js";
+import cors from "cors"
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/testedeploy",async (req,res) => {
+    res.status(200).send("TESTE")
+});
+
+tarefasController(app)
+usuariosController(app)
+
+export default app
